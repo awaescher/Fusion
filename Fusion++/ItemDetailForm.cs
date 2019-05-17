d3e5705bb5a1cba54133a84cea978ac589265a6e
Andreas Wäscher <awaescher@sodacore.net>
2019-05-18 00:22:09 +0200

    Clean up usings

--- Fusion++/ItemDetailForm.cs
+++ Fusion++/ItemDetailForm.cs
@@ -1,15 +1,8 @@
 ﻿using DevExpress.XtraEditors;
-using EasyScintilla.Stylers;
 using FusionPlusPlus.Model;
-using ScintillaNET;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FusionPlusPlus
