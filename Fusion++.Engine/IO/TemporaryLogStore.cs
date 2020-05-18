c4a99cd5d3b05a3e9d8838b0559c1763343ca822
Andreas Wäscher <awaescher@sodacore.net>
2020-05-18 22:03:26 +0200

    Set permissions to system users to the log path
    Fixes #14

--- Fusion++.Engine/IO/TemporaryLogStore.cs
+++ Fusion++.Engine/IO/TemporaryLogStore.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 namespace FusionPlusPlus.Engine.IO
 {
@@ -22,7 +24,63 @@ namespace FusionPlusPlus.Engine.IO
 		public void Prepare()
 		{
 			if (!Directory.Exists(Path))
+			{
 				Directory.CreateDirectory(Path);
+
+				try
+				{
+					SetFullControlPermissions(Path, WellKnownSidType.BuiltinUsersSid, WellKnownSidType.LocalSystemSid);
+				}
+				catch
+				{
+					// setting full control permissions is what we want to have for IIS logging, however we can 
+					// live without it: https://github.com/awaescher/Fusion/issues/14
+				}
+			}
+		}
+
+		static void SetFullControlPermissions(string path, params WellKnownSidType[] sids)
+		{
+			const FileSystemRights rights = FileSystemRights.FullControl;
+
+			foreach (var sid in sids)
+			{
+				bool result;
+				bool inheritedResult;
+
+				var securityIdentifier = new SecurityIdentifier(sid, null);
+
+				// Add Access Rule to the actual directory itself
+				var accessRule = new FileSystemAccessRule(
+					securityIdentifier,
+					rights,
+					InheritanceFlags.None,
+					PropagationFlags.NoPropagateInherit,
+					AccessControlType.Allow);
+
+				var info = new DirectoryInfo(path);
+				var security = info.GetAccessControl(AccessControlSections.Access);
+
+				security.ModifyAccessRule(AccessControlModification.Set, accessRule, out result);
+
+				if (!result)
+					throw new InvalidOperationException("Failed to give full-control permission to all users for path " + path);
+
+				// add inheritance
+				var inheritedAccessRule = new FileSystemAccessRule(
+					securityIdentifier,
+					rights,
+					InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit,
+					PropagationFlags.InheritOnly,
+					AccessControlType.Allow);
+
+				security.ModifyAccessRule(AccessControlModification.Add, inheritedAccessRule, out inheritedResult);
+
+				if (!inheritedResult)
+					throw new InvalidOperationException("Failed to give full-control permission inheritance to all users for " + path);
+
+				info.SetAccessControl(security);
+			}
 		}
 
 		public string GetLogName(string path)
